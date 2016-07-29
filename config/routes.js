7a9af4ac1d713ef936fdb6392532813e9aac3de6
Jeff Hedfors <git.j@hedfors.net>
2016-07-28 17:26:35 -0700

    completely rebuilt

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,1 @@
-var myApp = angular.module('myApp',['ngRoute','ngMessages'])
-myApp.config(function($routeProvider){
-  $routeProvider
-    .when('/',{
-      templateUrl:'partials/main.html'
-    })
-    .when('/projects',{
-      templateUrl:'partials/projects.html'
-    })
-    .otherwise({
-      redirectTo:'/'
-    })
-})
+var myApp = angular.module('myApp',['ngRoute','ngMessages','ui.materialize'])
