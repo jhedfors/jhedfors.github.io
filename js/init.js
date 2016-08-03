3ee5221e7157534ca4a7be0b3f052de98cc93eb3
Jeff Hedfors <git.j@hedfors.net>
2016-08-03 16:47:02 -0700

    update

--- js/init.js
+++ js/init.js
@@ -11,12 +11,12 @@
     $('a[href^="#p"]').on('click', function(event) {
 
         var target = $(this.getAttribute('href'));
-
+        console.log(target);
         if( target.length ) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 2000);
+            }, 1000);
         }
 
     });
